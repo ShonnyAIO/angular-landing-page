8cf8299d4072eeddb1a65ab3f8591a54ad86be73
Jonathan Torres <exodiakah@gmail.com>
2023-08-13 13:32:47 -0400

    Finished SetUp

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -5,7 +5,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.sass']
+  styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
 
